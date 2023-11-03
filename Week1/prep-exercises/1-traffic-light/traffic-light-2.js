9567a94af20985797940dd17c3c6db162e9afba4
Bilal Zalkhi <148480265+Bilalzalkhi@users.noreply.github.com>
2023-11-03 19:01:23 +0100

    Update traffic-light-2.js

--- Week1/prep-exercises/1-traffic-light/traffic-light-2.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-2.js
@@ -15,7 +15,8 @@ while (cycle < 2) {
   console.log("The traffic light is on", currentState);
   if(currentState === "green"){trafficLight.stateIndex = 1};
   else if (currentState === "orange"){trafficLight.stateIndex = 2};
-  else (currentState === "red"){trafficLight.stateIndex = 2 && cycle++};
+  else (currentState === "red"){trafficLight.stateIndex = 0};
+  cycle++;                             
     
    
   }
