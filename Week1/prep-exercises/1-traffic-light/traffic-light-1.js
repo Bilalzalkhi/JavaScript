0d2ce2b35913f6344680cf5a4e96fe1ad01b54e2
Bilal Zalkhi <148480265+Bilalzalkhi@users.noreply.github.com>
2023-11-03 18:52:35 +0100

    Update traffic-light-1.js

--- Week1/prep-exercises/1-traffic-light/traffic-light-1.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-1.js
@@ -11,9 +11,9 @@ let rotations = 0;
 while (rotations < 2) {
   const currentState = trafficLight.state;
   console.log("The traffic light is on", currentState);
-if (trafficLight.state == "green"){ trafficLight.state = "orange"};
-else if(trafficLight.state == "orange"){trafficLight.state = "red"};
-else if(trafficLight.state == "red"){trafficLight.state = "green"};  
+if (trafficLight.state === "green"){ trafficLight.state = "orange"};
+else if(trafficLight.state === "orange"){trafficLight.state = "red"};
+else if(trafficLight.state === "red"){trafficLight.state = "green"};  
 rotations++;  
   // TODO
   // if the color is green, turn it orange
